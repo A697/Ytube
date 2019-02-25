3476deb8bd2c0a1bdf7e738a3239ca3854de2a4b
A_97 <46586421+A697@users.noreply.github.com>
2019-02-25 15:34:24 +0530

    Initial commit

---
+++ app/src/main/java/com/example/ytube/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.ytube;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
+
+public class MainActivity extends YouTubeBaseActivity {
+
+    public static final String YT_API_KEY = "AIzaSyCkajA1NJ409YhDxlrRo3sTxycVLKbe7z0";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        YouTubePlayerView youTubePlayerView =
+                (YouTubePlayerView) findViewById(R.id.player);
+
+        youTubePlayerView.initialize(YT_API_KEY,
+                new YouTubePlayer.OnInitializedListener() {
+                    @Override
+                    public void onInitializationSuccess(YouTubePlayer.Provider provider,
+                                                        YouTubePlayer youTubePlayer, boolean b) {
+
+                        // do any work here to cue video, play video, etc.
+                        youTubePlayer.cueVideo("njvPKFWE3Rk");
+                        // or to play immediately
+                        // youTubePlayer.loadVideo("5xVh-7ywKpE");
+                    }
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult youTubeInitializationResult) {
+                        Toast.makeText(MainActivity.this, "Youtube Failed!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+    }
+}
